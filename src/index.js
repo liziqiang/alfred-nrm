bfec64e4c9f0a37a620d61ec55e48b865c92dd74
Ziqiang Li <ziqiang.lee@gmail.com>
2019-05-31 17:31:39 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const alfy = require('alfy');
 const util = require('./util');
+const STAR = '*';
+function isInUse(repo) {
+    return repo[0] === STAR;
+}
 
 util.getList().then((list) => {
-    let items = list.map((v) => {
-        const STAR = '*';
-        let isStar = v[0] === STAR;
-        let startIdx = isStar ? 1 : 0;
+    let items = alfy.matches(alfy.input, list, (item, input) => {
+        let inUse = isInUse(item);
+        return inUse ? item[1].includes(input) : item[0].includes(input);
+    }).map((v) => {
+        let inUse = isInUse(v);
+        let startIdx = inUse ? 1 : 0;
         let name = v[startIdx];
-        let repoUrl = v[startIdx + 1];
         return {
-            title: `${name} ${isStar ? STAR : ''}`,
+            title: `${name} ${inUse ? STAR : ''}`,
             arg: name,
-            subtitle: repoUrl,
-            variables: { name, repoUrl },
+            subtitle: v[startIdx + 1],
+            variables: { name },
             icon: {
                 path: alfy.icon.like
             }
